94f8e1a830cd1f8819a42cb4b5075594b8624e64
HMilbradt <harrison.milbradt@gmail.com>
2018-02-14 11:35:29 -0800

    Added title for table in template, fixed base controller bug

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -11,8 +11,8 @@ class Views extends Application
 
         // so we don't need pagebody
         $this->data['content'] = 'Ok';
-        $this->data['leftside'] = 'by_priority';
-        $this->data['rightside'] = 'by_category';
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
 
         $this->render('template_secondary');
     }
