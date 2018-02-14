3572d91ea369b15682fe7e477d2526f1944ea91f
HMilbradt <harrison.milbradt@gmail.com>
2018-02-14 11:52:40 -0800

    Added template files for priority and category, added helper functions to Views controller

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -16,4 +16,44 @@ class Views extends Application
 
         $this->render('template_secondary');
     }
+
+    function makePrioritizedPanel($tasks) {
+
+        // extract the undone tasks
+        foreach ($tasks as $task)
+        {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
+
+        // order them by priority
+        usort($undone, "orderByPriority");
+
+        // substitute the priority name
+        foreach ($undone as $task)
+        {
+            $task->priority = $this->app->priority($task->priority);
+        }
+
+        // convert the array of task objects into an array of associative objects
+        foreach ($undone as $task)
+        {
+            $converted[] = (array) $task;
+        }
+
+        // and then pass them on
+        $parms = ['display_tasks' => $converted];
+        return $this->parser->parse('by_priority', $parms, true);
+    }
+}
+
+// return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
+function orderByPriority($a, $b)
+{
+    if ($a->priority > $b->priority)
+        return -1;
+    elseif ($a->priority < $b->priority)
+        return 1;
+    else
+        return 0;
 }
